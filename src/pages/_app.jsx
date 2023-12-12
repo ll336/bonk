3d98064d3ec120fe9b80e0a770a657061d882ada
Leon <leon@da-tui.com>
2023-12-13 00:22:59 +0800

    max-h-screen

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,7 +2,7 @@ import "../styles/globals.css"
 import React, { useState, useEffect } from "react";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { cn } from "@/lib/utils";
+import { cn, useWindowSize } from "@/lib/utils";
 import "../styles/styles.css"
 
 
@@ -11,7 +11,8 @@ import "../styles/styles.css"
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   const [mounted, setMounted] = useState(false);
- 
+  const windowSize = useWindowSize()
+  const shouldApplyScroll = windowSize.height <= 650
   
 
   useEffect(() => {
@@ -29,7 +30,7 @@ export default function App({ Component, pageProps: { session, ...pageProps } })
 <>
         <div 
         className={cn(
-          `lg:max-h-screen h-screen lg:overflow-hidden overflow-y-auto max-h-full lg:w-full relative`
+          `h-screen ${shouldApplyScroll ? "lg:overflow-y-auto lg:max-h-full" : "lg:overflow-hidden lg:max-h-screen"} overflow-y-auto max-h-full lg:w-full relative`
         )}>
 
 
